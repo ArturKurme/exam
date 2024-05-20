f82a8361f884d2a76fd0b052782bf1bbd5543704
Arturs Kurme <artur.kurme@gmail.com>
2024-05-20 14:24:23 +0300

    First template

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Flask!</p>"
+    return render_template('main.html', title='Hello, Flask!')
