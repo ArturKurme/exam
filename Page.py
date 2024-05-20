7ded865bfca067a8f3e5495e0acd776beec9499d
Arturs Kurme <artur.kurme@gmail.com>
2024-05-20 18:16:36 +0300

    Navigation bar

---
+++ Page.py
@@ -0,0 +1,15 @@
+class Page:
+    def __init__(self, code, title, href):
+        self.code = code
+        self.title = title
+        self.href = href
+
+
+all_pages = (
+    Page('home', 'Home', '/home'),
+    Page('artists', 'Bands', '/artists'),
+)
+
+
+def current_page_title(nav_current):
+    return next((page.title for page in all_pages if page.code == nav_current), 'Hello!')
--- application.py
+++ application.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from flask import render_template
 
+from Page import current_page_title, all_pages
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return render_template('main.html', title='Hello, Flask!')
+@app.route("/home")
+def main():
+    nav_current = 'home'
+    return render_template('main.html',
+                           title=current_page_title(nav_current),
+                           nav_current=nav_current,
+                           nav_items=all_pages)
+
+
+@app.route("/artists")
+def artists():
+    nav_current = 'artists'
+    return render_template('main.html',
+                           title=current_page_title(nav_current),
+                           nav_current=nav_current,
+                           nav_items=all_pages)
