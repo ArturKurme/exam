c66adc0f867b94255cbdfb8b244f2b29053fc7fa
Arturs Kurme <artur.kurme@gmail.com>
2024-05-21 12:46:22 +0300

    Band page

--- Band.py
+++ Band.py
@@ -5,7 +5,7 @@ import json
 class Band:
 
     def __init__(self, file_name):
-        with open(file_name) as f:
+        with open(file_name, encoding='utf-8') as f:
             obj = json.load(f)
         self.code = obj['code']
         self.title = obj['title']
@@ -13,7 +13,11 @@ class Band:
 
     @property
     def logo(self):
-        return 'static/images/' + self.code + '_logo.jpg'
+        return '/static/images/' + self.code + '_logo.jpg'
+
+    @property
+    def href(self):
+        return '/band/' + self.code
 
 
 all_bands = {band.code: band for band in [Band(path) for path in glob.glob("data/bands/*.json")]}
--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 
 from Band import all_bands
-from Page import current_page_title, all_pages
+from Page import current_page_title, all_pages, Page
 
 app = Flask(__name__)
 
@@ -22,6 +22,16 @@ def bands():
     nav_current = 'bands'
     return render_template('bands.html',
                            title=current_page_title(nav_current),
-                           bands=sorted(all_bands, key=lambda band: band.title),
+                           bands=sorted(all_bands.values(), key=lambda band: band.title),
                            nav_current=nav_current,
                            nav_items=all_pages)
+
+
+@app.route("/band/<band_code>")
+def band_page(band_code):
+    band = all_bands[band_code]
+    return render_template('band_page.html',
+                           title=band.title,
+                           band=band,
+                           nav_current='band',
+                           nav_items=all_pages + (Page('band', band.title, '#'),))
