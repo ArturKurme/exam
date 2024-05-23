ad7f49e54cea2c1857b1e0b8a1403d24f56e31c9
Arturs Kurme <artur.kurme@gmail.com>
2024-05-23 14:15:07 +0300

    Tracks

--- Album.py
+++ Album.py
@@ -1,9 +1,22 @@
 import csv
 import glob
 import os
-from datetime import date
+from datetime import date, timedelta
+
+from pytimeparse.timeparse import timeparse
 
 from Item import Item
+
+
+class Track:
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+    @property
+    def length(self):
+        total_seconds = int(self.duration.total_seconds())
+        return f'{int(total_seconds / 60) % 60}:{total_seconds % 60:02}'
 
 
 class Album(Item):
@@ -11,6 +24,7 @@ class Album(Item):
     def __init__(self, code, released, title, description=''):
         super().__init__(code, title, description)
         self.released = released
+        self.tracks = []
 
     @property
     def year(self):
@@ -20,6 +34,36 @@ class Album(Item):
     def cover(self):
         paths = glob.glob('static/images/' + self.code + '_cover.*')
         return '/static/images/' + (os.path.basename(paths[0]) if paths else 'no_image.jpg')
+
+    @property
+    def duration(self):
+        return sum([track.duration for track in self.tracks], start=timedelta(0))
+
+    @property
+    def length(self):
+        total_seconds = int(self.duration.total_seconds())
+        return f'{int(total_seconds / 60) % 60}:{total_seconds % 60:02}'
+
+
+def read_tracks(code):
+    tracks = []
+    for path in glob.glob('data/albums/' + code + '.csv'):
+        try:
+            band = os.path.splitext(os.path.basename(path))[0]
+            with open("data/albums/" + code + ".csv", encoding='utf-8', newline='') as csv_file:
+                reader = csv.DictReader(csv_file, delimiter=',')
+                for row in reader:
+                    try:
+                        duration = timedelta(seconds=timeparse(row['length']))
+                        title = row['title']
+                        if title and duration:
+                            tracks.append(Track(title, duration))
+                    except Exception as ex:
+                        print(f"${type(ex)}: {ex} path: {path} {row}")
+        except Exception as ex:
+            print(f"${type(ex)}: {ex} path: {path}")
+
+    return tracks
 
 
 def read_discography():
@@ -37,8 +81,8 @@ def read_discography():
                         released = date.fromisoformat(row['released'])
                         if code and title:
                             album = Album(code, released, title, row['description'])
+                            album.tracks = read_tracks(code)
                             discography[band].append(album)
-                            album.cover
                     except Exception as ex:
                         print(f"${type(ex)}: {ex} path: {path} {row}")
         except Exception as ex:
